60083e424de00dd8aefe40a76da54a2e53a5e93a
travor <travor20814@gmail.com>
2016-07-26 16:06:52 +0800

    切版

--- app/src/main/java/ntpu_dmcl/ntpu_guide/MainActivity.java
+++ app/src/main/java/ntpu_dmcl/ntpu_guide/MainActivity.java
@@ -1,13 +1,20 @@
 package ntpu_dmcl.ntpu_guide;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WebView map ;
         setContentView(R.layout.activity_main);
+
+        map = (WebView)findViewById(R.id.navigationMap);
+        map.getSettings().setJavaScriptEnabled(true);
+        map.loadUrl("file:///android_asset/GMap.html");
     }
 }
