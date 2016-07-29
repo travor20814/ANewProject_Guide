7e672ac9540a8dff7fff6ee4f7c03920c00c4b6a
陳澔偉 <f745311@gmail.com>
2016-07-29 22:42:23 +0800

    add drawer

---
+++ app/src/main/java/ntpu_dmcl/ntpu_guide/mainActivity_drawer.java
@@ -0,0 +1,18 @@
+package ntpu_dmcl.ntpu_guide;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by chenhaowei on 2016/7/29.
+ */
+
+public class mainActivity_drawer extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.main_drawer, container, false);
+        return v;
+    }
+}
